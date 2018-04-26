94da85033e98fd24e57145327e22ed5edaee545f
robjweiss <rweiss55@gmail.com>
2018-04-26 00:16:17 -0400

    removed blanks from CSV

--- utils/removeErrorLines.py
+++ utils/removeErrorLines.py
@@ -15,12 +15,13 @@ with open('../data/errorLines.txt', 'r') as errorLines:
 print(len(listOfLineNums)) #result was 429
 
 
-oldFile = open('../data/imdb.backup', 'r')
+oldFile = open('../data/imdb.csv', 'r')
 oldLines = oldFile.readlines()
 oldFile.close()
 
 indexOfBadLines = 0
 numberOfNonFloats = 0
+numberOfBlanks = 0
 
 newFile = open('../data/imdb.csv', 'w')
 
@@ -28,33 +29,18 @@ for index, line in enumerate(oldLines):
     #print(listOfLineNums[indexOfBadLines])
     currentLineArray = line.split(',')
     if index == int(listOfLineNums[indexOfBadLines]):
-        print(indexOfBadLines)
         if int(listOfLineNums[indexOfBadLines]) < 14688:
             indexOfBadLines += 1
     elif '\\' in line:
         line = line.replace('\\', '')
         #newFile.write(line)
         numberOfNonFloats += 1
-        print('Non-Float detected: ' + str(numberOfNonFloats))
+    elif currentLineArray[0] == '' or currentLineArray[1] == '' or currentLineArray[2] == '':
+        numberOfBlanks += 1
     else:
         newFile.write(line)
+print('Error reported lines : ' + str(indexOfBadLines))
+print('Non-Float detected: ' + str(numberOfNonFloats))
+print('Blanks: ' + str(numberOfBlanks))
+print('Total: ' + str(indexOfBadLines + numberOfNonFloats + numberOfBlanks))
 newFile.close()
-
-# for n in listOfLineNums:
-#     for line in fileinput.input('../data/imdb.csv', inplace=True):
-#         if fileinput.lineno() == n:
-#             continue
-#             print(line, end='')
-
-
-# def removeLine(n, f):
-#     d = f.readlines()
-#     f.seek(0)
-#     for i in range(len(d)):
-#         f.write(d[i])
-#     f.truncate()
-#
-# f = open('../data/imdb.csv',"r+")
-# for lineToRemove in listOfLineNums:
-#     removeLine(lineToRemove, f)
-# f.close()
