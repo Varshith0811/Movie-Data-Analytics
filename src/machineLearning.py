d994194af1f1179928d9a469e31064e918bf95a9
Kevin Hockenjos <slyster1010@gmail.com>
2018-04-25 21:39:12 -0400

    ok

--- src/machineLearning.py
+++ src/machineLearning.py
@@ -6,17 +6,17 @@ import numpy as np
 
 '''Actual:'''
 
-features = pd.read_csv('../data/imdb.csv', error_bad_lines=False)
+features = pd.read_csv('../data/imdb.csv', error_bad_lines=False, engine='python')
 
 #One-Hot Encoding
-features = pd.get_dummies(features)
+features = pd.get_dummies(features, sparse=True)
 
 # Labels are the values we want to predict
-labels = np.array(features['actual'])
+labels = np.array(features['imdbRating'])
 
 # Remove the labels from the features
 # axis 1 refers to the columns
-features= features.drop('actual', axis = 1)
+features= features.drop('imdbRating', axis = 1)
 
 # Saving feature names for later use
 feature_list = list(features.columns)
